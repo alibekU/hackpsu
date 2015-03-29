dbf72dc7f47a01ae09d8bc5978a988bf2243a0d3
DaniyarYeralin <daniyar.yeralin@gmail.com>
2015-03-28 21:56:59 -0400

    Front End Upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+var marker, map, lat, lng, pos;
+function initialize() {
+  var mapOptions = {
+    zoom: 8
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  map.setOptions({disableDoubleClickZoom: true});
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    map.setCenter({ lat: 40.79464329620785, lng: -77.85736083984375});
+    handleNoGeolocation(false);
+  }
+
+  google.maps.event.addListener(map, 'click', function(e) {
+    placeMarker(e.latLng, map);
+  });
+  function placeMarker(position, map) {
+    if(marker != null)
+      marker.setMap(null);
+    marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
+    map.panTo(position);
+    document.getElementById("latbox").value = marker.getPosition().lat();
+    document.getElementById("lngbox").value = marker.getPosition().lng();
+  }
+}
+google.maps.event.addDomListener(window, 'load', initialize);
